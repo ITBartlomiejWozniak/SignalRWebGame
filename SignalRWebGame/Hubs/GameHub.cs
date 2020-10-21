a8e9fbdcadc123d9557cd444c04af3be9181b15d
Bartek Woźniak <bartekwozniak93@gmail.com>
2020-10-21 14:19:39 +0200

    Add SignalR Implement GameHub

---
+++ SignalRWebGame/Hubs/GameHub.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace SignalRWebGame.Hubs
+{
+    public class GameHub : Hub
+    {
+        public async Task MoveBall(string y, string x)
+        {
+            await Clients.All.SendAsync("moved", y, x);
+        }
+    }
+}
